a91f3a19b8797a72bcb4443e35f1781b1344650f
ninja <ninja@nicos-mbp.localdomain>
2024-04-15 17:22:05 +0200

    initial commit

---
+++ src/main/java/ninja/b1t/JEEBackendAPI/JeeBackendApiApplication.java
@@ -0,0 +1,13 @@
+package ninja.b1t.JEEBackendAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeeBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ninja/b1t/JEEBackendAPI/JeeBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ninja.b1t.JEEBackendAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
